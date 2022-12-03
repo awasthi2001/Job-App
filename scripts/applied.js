6826c27ac39c08d13d9a3264d370c2d5599ccad1
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-03 19:54:33 +0530

    working on applied.js

--- scripts/applied.js
+++ scripts/applied.js
@@ -4,3 +4,9 @@ import {Navbar} from '../Components/Navbar.js'
 let navbar = document.getElementById('links');
 
 navbar.innerHTML = Navbar();
+
+let AppliedJobs = JSON.parse(localStorage.getItem('joblists')) || [];
+
+function DisplayItems(){
+    
+}

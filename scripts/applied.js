735fffd07751a5eb0418534b039d1df1f40a370f
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-04 16:32:13 +0530

    working on applied.js

--- scripts/applied.js
+++ scripts/applied.js
@@ -6,7 +6,12 @@ let navbar = document.getElementById('links');
 navbar.innerHTML = Navbar();
 
 let AppliedJobs = JSON.parse(localStorage.getItem('joblists')) || [];
-
+DisplayItems()
 function DisplayItems(){
-    
+    document.getElementById('body').innerHTML="";
+    AppliedJobs.map(({name,email,JobType,expected})=>{
+
+    })
 }
+
+console.log(AppliedJobs)

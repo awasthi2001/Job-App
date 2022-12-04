1c111ada83dea4f52e1b50c7dc9dfa99bcea935e
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-04 21:52:10 +0530

    done

--- scripts/applied.js
+++ scripts/applied.js
@@ -1,100 +1,101 @@
-
-
-import {Navbar} from '../Components/Navbar.js'
-let navbar = document.getElementById('links');
+import { Navbar } from "../Components/Navbar.js";
+let navbar = document.getElementById("links");
 
 navbar.innerHTML = Navbar();
 
-let AppliedJobs = JSON.parse(localStorage.getItem('joblists')) || [];
-DisplayItems(AppliedJobs)
-function DisplayItems(AppliedJobs){
-    let tbody = document.getElementById('body')
-    tbody.innerHTML="";
-    AppliedJobs.map(({name,email,JobType,expected},index)=>{
-       let row = document.createElement("tr");
-       row.innerHTML = `
+let AppliedJobs = JSON.parse(localStorage.getItem("joblists")) || [];
+DisplayItems(AppliedJobs);
+function DisplayItems(AppliedJobs) {
+  let tbody = document.getElementById("body");
+  tbody.innerHTML = "";
+  AppliedJobs.map(({ name, email, JobType, expected }, index) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
        <td>${name}</td>
        <td>${email}</td>
        <td>${JobType}</td>
        <td>${expected}</td>
-       `
-       let btn = document.createElement('button');
-       btn.innerText = "ADD TO BOOKMARK"
-       btn.style.color = 'green';
-       btn.style.fontWeight = 'bold';
-       btn.style.margin = '0px'
-       btn.style.marginTop = '10px'
-       btn.style.marginLeft = '50px'
-       btn.style.cursor = 'pointer'
-       btn.style.backgroundColor ='transparent';
-       btn.style.border = 'none'
-       btn.addEventListener('click',function(){AddToBookmark(name,email,JobType,expected,index)})
-       row.append(btn);
-       tbody.append(row);
-    })
+       `;
+    let btn = document.createElement("button");
+    btn.innerText = "ADD TO BOOKMARK";
+    btn.style.color = "green";
+    btn.style.fontWeight = "bold";
+    btn.style.margin = "0px";
+    btn.style.marginTop = "10px";
+    btn.style.marginLeft = "50px";
+    btn.style.cursor = "pointer";
+    btn.style.backgroundColor = "transparent";
+    btn.style.border = "none";
+    btn.addEventListener("click", function () {
+      AddToBookmark(name, email, JobType, expected, index);
+    });
+    row.append(btn);
+    tbody.append(row);
+  });
 }
 
-let Bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
-function AddToBookmark(name,email,JobType,expected,index){
-let jobobj = {
-    name : name,
-    email : email,
-    JobType : JobType,
-     expected : expected,
-}
-Bookmarks.push(jobobj);
-localStorage.setItem('bookmarks',JSON.stringify(Bookmarks));
-alert('Added To Bookmarks')
+let Bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+function AddToBookmark(name, email, JobType, expected, index) {
+  let jobobj = {
+    name: name,
+    email: email,
+    JobType: JobType,
+    expected: expected,
+  };
+  Bookmarks.push(jobobj);
+  localStorage.setItem("bookmarks", JSON.stringify(Bookmarks));
+  alert("Added To Bookmarks");
 }
 let dummyArr = AppliedJobs;
-let SortByNames = document.getElementById('sortNames');
-SortByNames.addEventListener('change',handleNamesSort)
-function handleNamesSort(){
-    let order = document.getElementById('sortNames').value;
-    if(order==='ascending'){
-        dummyArr.sort(function(a,b) {
-            if(a.name>b.name)return 1;
-            if(a.name<b.name)return -1;
-            return 0;
-        })
-        }
-        if(order==='descending'){
-        dummyArr.sort(function(a,b) {
-            if(a.name>b.name)return -1;
-            if(a.name<b.name)return 1;
-            return 0;
-        })
-   
-        }
-        
-        DisplayItems(dummyArr);
+let SortByNames = document.getElementById("sortNames");
+SortByNames.addEventListener("change", handleNamesSort);
+function handleNamesSort() {
+  let order = document.getElementById("sortNames").value;
+  if (order === "ascending") {
+    dummyArr.sort(function (a, b) {
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+      return 0;
+    });
+  }
+  if (order === "descending") {
+    dummyArr.sort(function (a, b) {
+      if (a.name > b.name) return -1;
+      if (a.name < b.name) return 1;
+      return 0;
+    });
+  }
+
+  DisplayItems(dummyArr);
 }
 
-document.querySelector("#filterrole").addEventListener("change",handlefilter);
-function handlefilter(){
-  var role =  document.querySelector("#filterrole").value;    
-dummyArr = AppliedJobs.filter(function(elem,index){
-  if(document.querySelector("#filterrole").value=="All" || role==""){
- return 1;
-}else{ 
- return elem.JobType==role;
-}
-})
-DisplayItems(dummyArr);
-}
-
-document.querySelector("#sortSalary").addEventListener('change',handleSortBySalary);
-
-function handleSortBySalary(){
-    let order = document.querySelector("#sortSalary").value;
-    if(order=='lth'){
-        dummyArr.sort(function(a,b){
-            return a.expected-b.expected;
-        })
-    }else{
-        dummyArr.sort(function(a,b){
-            return b.expected-a.expected;
-        })
+document.querySelector("#filterrole").addEventListener("change", handlefilter);
+function handlefilter() {
+  var role = document.querySelector("#filterrole").value;
+  dummyArr = AppliedJobs.filter(function (elem, index) {
+    if (document.querySelector("#filterrole").value == "All" || role == "") {
+      return 1;
+    } else {
+      return elem.JobType == role;
     }
-    DisplayItems(dummyArr)
+  });
+  DisplayItems(dummyArr);
+}
+
+document
+  .querySelector("#sortSalary")
+  .addEventListener("change", handleSortBySalary);
+
+function handleSortBySalary() {
+  let order = document.querySelector("#sortSalary").value;
+  if (order == "lth") {
+    dummyArr.sort(function (a, b) {
+      return a.expected - b.expected;
+    });
+  } else {
+    dummyArr.sort(function (a, b) {
+      return b.expected - a.expected;
+    });
+  }
+  DisplayItems(dummyArr);
 }
--- scripts/bookmark.js
+++ scripts/bookmark.js
@@ -1,41 +1,40 @@
-
-
-import {Navbar} from '../Components/Navbar.js'
-let navbar = document.getElementById('links');
+import { Navbar } from "../Components/Navbar.js";
+let navbar = document.getElementById("links");
 
 navbar.innerHTML = Navbar();
 
-let Bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
-DisplayItems()
-function DisplayItems(){
-    let tbody = document.getElementById('body')
-    tbody.innerHTML="";
-    Bookmarks.map(({name,email,JobType,expected},index)=>{
-       let row = document.createElement("tr");
-       row.innerHTML = `
+let Bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+DisplayItems();
+function DisplayItems() {
+  let tbody = document.getElementById("body");
+  tbody.innerHTML = "";
+  Bookmarks.map(({ name, email, JobType, expected }, index) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
        <td>${name}</td>
        <td>${email}</td>
        <td>${JobType}</td>
-       `
-       let btn = document.createElement('button');
-       btn.innerText = "Delete"
-       btn.style.color = 'red';
-       btn.style.fontWeight = 'bold';
-       btn.style.margin = '0px'
-       btn.style.marginTop = '10px'
-       btn.style.marginLeft = '50px'
-       btn.style.cursor = 'pointer'
-       btn.style.backgroundColor ='transparent';
-       btn.style.border = 'none'
-       btn.addEventListener('click',function(){RemoveFromBookmark(name,email,JobType,expected,index)})
-       row.append(btn);
-       tbody.append(row);
-    })
+       `;
+    let btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.style.color = "red";
+    btn.style.fontWeight = "bold";
+    btn.style.margin = "0px";
+    btn.style.marginTop = "10px";
+    btn.style.marginLeft = "50px";
+    btn.style.cursor = "pointer";
+    btn.style.backgroundColor = "transparent";
+    btn.style.border = "none";
+    btn.addEventListener("click", function () {
+      RemoveFromBookmark(name, email, JobType, expected, index);
+    });
+    row.append(btn);
+    tbody.append(row);
+  });
 }
 
-
-function RemoveFromBookmark(index){
-Bookmarks.splice(index,1);
-localStorage.setItem('bookmarks',JSON.stringify(Bookmarks));
-DisplayItems();
+function RemoveFromBookmark(index) {
+  Bookmarks.splice(index, 1);
+  localStorage.setItem("bookmarks", JSON.stringify(Bookmarks));
+  DisplayItems();
 }

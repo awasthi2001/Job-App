ffa0ac9c2b1888940ee6b6ee7c4312dc72fa6076
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-04 20:51:53 +0530

    working on Js part of applied.js

--- scripts/applied.js
+++ scripts/applied.js
@@ -8,9 +8,28 @@ navbar.innerHTML = Navbar();
 let AppliedJobs = JSON.parse(localStorage.getItem('joblists')) || [];
 DisplayItems()
 function DisplayItems(){
-    document.getElementById('body').innerHTML="";
+    let tbody = document.getElementById('body')
+    tbody.innerHTML="";
     AppliedJobs.map(({name,email,JobType,expected})=>{
-
+       let row = document.createElement("tr");
+       row.innerHTML = `
+       <td>${name}</td>
+       <td>${email}</td>
+       <td>${JobType}</td>
+       <td>${expected}</td>
+       `
+       let btn = document.createElement('button');
+       btn.innerText = "ADD TO BOOKMARK"
+       btn.style.color = 'green';
+       btn.style.fontWeight = 'bold';
+       btn.style.margin = '0px'
+       btn.style.marginTop = '10px'
+       btn.style.marginLeft = '50px'
+       btn.style.cursor = 'pointer'
+       btn.style.backgroundColor ='transparent';
+       btn.style.border = 'none'
+       row.append(btn);
+       tbody.append(row);
     })
 }
 
